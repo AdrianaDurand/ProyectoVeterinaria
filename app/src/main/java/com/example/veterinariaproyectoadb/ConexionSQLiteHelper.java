4d1ab1a6c0d21b63d41b33a27ff696d37d5fc065
Adriana Durand <adrianadurand828@gmail.com>
2023-09-29 08:47:29 -0500

    Registrar Dueños/Mascotas Terminado

--- app/src/main/java/com/example/veterinariaproyectoadb/ConexionSQLiteHelper.java
+++ app/src/main/java/com/example/veterinariaproyectoadb/ConexionSQLiteHelper.java
@@ -1,2 +1,54 @@
-package com.example.veterinariaproyectoadb;public class ConexionSQLiteHelper {
+package com.example.veterinariaproyectoadb;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ConexionSQLiteHelper extends SQLiteOpenHelper {
+
+
+    final String MASCOTAS = "" +
+            "CREATE TABLE 'mascotas'(" +
+            "'idmascota'       INTEGER NOT NULL," +
+            "'nombre'           TEXT NOT NULL," +
+            "'tipo'           TEXT NOT NULL," +
+            "'raza'           TEXT NOT NULL," +
+            "'peso'           TEXT NOT NULL," +
+            "'color'           TEXT NOT NULL," +
+            "PRIMARY KEY     ('idmascota' AUTOINCREMENT)"+
+            ")";
+
+
+    final String DUENOS = "" +
+            "CREATE TABLE 'duenos'(" +
+            "'iddueno'          INTEGER NOT NULL," +
+            "'apellidos'        TEXT NOT NULL," +
+            "'nombres'          TEXT NOT NULL," +
+            "'fechanacimiento'  TEXT NOT NULL," +
+            "'telefono'         TEXT NOT NULL," +
+            "'email'            TEXT NOT NULL," +
+            "'direccion'        TEXT NOT NULL," +
+            "PRIMARY KEY     ('iddueno' AUTOINCREMENT)"+
+            ")";
+
+    public ConexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(MASCOTAS);
+        sqLiteDatabase.execSQL(DUENOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS mascotas");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS duenos");
+        onCreate(sqLiteDatabase);
+
+    }
+
 }
--- app/src/main/java/com/example/veterinariaproyectoadb/registrarduenos.java
+++ app/src/main/java/com/example/veterinariaproyectoadb/registrarduenos.java
@@ -1,14 +1,130 @@
 package com.example.veterinariaproyectoadb;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registrarduenos extends AppCompatActivity {
-
+    EditText etApellidos, etNombres, etFechanac, etTelefono, etEmail, etDireccion;
+    Button btRegistrarDueños, btAbrirBusqueda, btAbrirLista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarduenos);
+
+        loadUI();
+
+        btRegistrarDueños.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarCampos();
+            }
+        });
+
+        /*btAbrirBusqueda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), buscarClientes.class));
+            }
+        });*/
+    }
+    private void validarCampos(){
+        String apellidos, nombres, fechanacimiento, telefono, email, direccion;
+
+        apellidos           = etApellidos.getText().toString();
+        nombres             = etNombres.getText().toString();
+        fechanacimiento     = etFechanac.getText().toString();
+        telefono            = etTelefono.getText().toString();
+        email               = etEmail.getText().toString();
+        direccion           = etDireccion.getText().toString();
+
+        if(apellidos.isEmpty() || nombres.isEmpty() || apellidos.isEmpty() || fechanacimiento.isEmpty() || telefono.isEmpty() || email.isEmpty() || direccion.isEmpty()){
+            notificar("Complete el formulario");
+            etApellidos.requestFocus();
+        }else{
+            preguntar();
+        }
+
+    }
+
+    private void preguntar() {
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+        dialogo.setTitle("VETERINARIA");
+        dialogo.setMessage("¿Está seguro de registrar a este dueño?");
+        dialogo.setCancelable(false);
+
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                registrarDueno();}
+        });
+
+        dialogo.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialogo.show();
+    }
+
+    private void registrarDueno(){
+
+        ConexionSQLiteHelper conexion = new ConexionSQLiteHelper(this,"dbveterinaria", null, 1);
+        SQLiteDatabase db = conexion.getReadableDatabase();
+
+
+        ContentValues parametros = new ContentValues();
+        parametros.put("apellidos", etApellidos.getText().toString());
+        parametros.put("nombres", etNombres.getText().toString());
+        parametros.put("fechanacimiento", etFechanac.getText().toString());
+        parametros.put("telefono", etTelefono.getText().toString());
+        parametros.put("email", etEmail.getText().toString());
+        parametros.put("direccion", etDireccion.getText().toString());
+
+
+        long idobtenido = db.insert("duenos", "iddueno", parametros);
+        notificar("Registro guardado - " + String.valueOf(idobtenido));
+        reiniciar();
+        etApellidos.requestFocus();
+    }
+
+    private void reiniciar(){
+        etApellidos.setText(null);
+        etNombres.setText(null);
+        etFechanac.setText(null);
+        etTelefono.setText(null);
+        etEmail.setText(null);
+        etDireccion.setText(null);
+    }
+
+    private void notificar(String mensaje){
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
+    }
+
+    private void loadUI(){
+        etApellidos = findViewById(R.id.etApellidos);
+        etNombres = findViewById(R.id.etNombres);
+        etFechanac = findViewById(R.id.etFechanac);
+        etTelefono = findViewById(R.id.etTelefono);
+        etEmail = findViewById(R.id.etEmail);
+        etDireccion = findViewById(R.id.etDireccion);
+
+        btRegistrarDueños = findViewById(R.id.btRegistrarDueños);
+        btAbrirBusqueda = findViewById(R.id.btAbrirBusqueda);
+
+
+
     }
 }
+
+
