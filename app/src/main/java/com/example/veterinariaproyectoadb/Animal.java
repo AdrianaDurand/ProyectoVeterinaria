dc777fa4489e61efa44944d064b3b2b4639ecc20
Adriana Durand <adrianadurand828@gmail.com>
2023-09-29 17:30:06 -0500

    Listar Mascotar terminado

---
+++ app/src/main/java/com/example/veterinariaproyectoadb/Animal.java
@@ -0,0 +1,2 @@
+package com.example.veterinariaproyectoadb;public class Animal {
+}
---
+++ app/src/main/java/com/example/veterinariaproyectoadb/listarMascotas.java
@@ -0,0 +1,14 @@
+package com.example.veterinariaproyectoadb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listarMascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_mascotas);
+    }
+}
